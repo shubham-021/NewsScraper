d6b86e66bf897b537c53a766a76bf0bda2ba7367
Arka <shubhamthesingh21@gmail.com>
2025-05-11 15:06:13 +0530

    main added

---
+++ Server/ts/main.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import { PrismaClient } from '../src/generated/prisma'
+import Cors from 'cors'
+
+const prisma = new PrismaClient
+
+const app = express()
+app.use(Cors())
+app.use(express.json())
+
+app.get('/api/articles/:date', async (req, res) => {
+  const requestedDate = new Date(req.params.date);
+  const dateString = requestedDate.toISOString().split('T')[0];
+  const limit = parseInt(req.query.limit as string, 10) || 8;
+
+  try {
+    const articles = await prisma.headlines.findMany();
+
+    const filtered = articles.filter(article =>
+        article.date.toISOString().split('T')[0] === dateString
+    );
+
+
+    res.json(filtered.slice(0,limit));
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.listen(3000 , () => {
+    console.log("Server running successfully")
+})
